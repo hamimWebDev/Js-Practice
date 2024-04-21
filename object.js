7910f4f623fc63a7cfe4c0471f098ce2fb52bf48
hamimDev <mdhamimhowladerasif@gmail.com>
2024-04-21 23:35:00 +0600

    access object property

--- object.js
+++ object.js
@@ -7,5 +7,11 @@ const bottle = {
   brand: "apple",
   price: 45,
   color: "white",
-  isClean: true,
+  "is-Clean": true,
 };
+
+// access object property
+
+console.log(bottle.price);
+console.log(bottle["color"]);
+console.log(bottle["is-Clean"]);
