099099e32afe12f48c69856df2c876837867ee1f
hamimDev <mdhamimhowladerasif@gmail.com>
2024-04-21 21:08:13 +0600

    toUpperCase && toLowerCase

---
+++ uppercaseLowercase.js
@@ -0,0 +1,19 @@
+const subject = "chemistry";
+const book = "chemIsTry";
+
+// lowercase
+
+if (subject.toLowerCase() === book.toLowerCase()) {
+  console.log("pass korbo");
+} else {
+  console.log("fall korbo");
+}
+
+// Uppercase
+
+if (subject.toUpperCase() === book.toUpperCase()) {
+  console.log("pass korbo");
+} else {
+  console.log("fall korbo");
+}
+
