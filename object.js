631a9d7188b2b7721feb2d7d8330e45438d4f678
hamimDev <mdhamimhowladerasif@gmail.com>
2024-04-21 22:43:47 +0600

    object practice

---
+++ object.js
@@ -0,0 +1,11 @@
+const age = 21;
+const school = "A.K. school";
+const isPassed = true;
+// let isDeveloper;
+
+const bottle = {
+  brand: "apple",
+  price: 45,
+  color: "white",
+  isClean: true,
+};
