58e33ec1577bb42571c3e17db1eabab5b2ad050d
hamimDev <mdhamimhowladerasif@gmail.com>
2024-04-22 08:37:02 +0600

    nested practice

---
+++ nested.js
@@ -0,0 +1,19 @@
+const college = {
+  name: "Mahelara Degree College",
+  class: [11, 12],
+  unique: {
+    color: "blue",
+    result: {
+      gpa: 5,
+      merit: "top",
+    },
+  },
+};
+
+// console.log(college.name);
+
+// console.log(college.unique.color);
+
+// console.log(college.unique.result.merit);
+
+// console.log(college.unique.result["gpa"]);
